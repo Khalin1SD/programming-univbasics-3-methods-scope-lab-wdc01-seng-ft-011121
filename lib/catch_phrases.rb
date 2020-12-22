7337b7e8bd812261ee934d27d5de3ec385c76941
calm-collection-3916 <khalinredd@gmail.com>
2020-12-22 00:46:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -19,6 +19,5 @@ def all_phrases(mario, toadstool, link)
   mario = "It's-a me, Mario!"
   toadstool = "Thank You Mario! But Our Princess Is In Another Castle!"
   link = "It's Dangerous To Go Alone! Take This."
+  return all_phrases
 end
-
-return all_phrases
